897e0492bfbe3e87d90c612c397f24b1baaa4d81
Samuel J. Herold <sherold@uw.edu>
2026-01-22 16:57:48 -0800

    "Initial upload of clean_sonoma"

---
+++ clean_sonoma.R
@@ -0,0 +1,36 @@
+#### Script to clean SONOMA csv data ####
+
+
+
+#### SETTINGS ####
+
+# Put your path to the SONOMA csv here 
+#(e.g. //biostat-fs2-s/users/sherold/Documents/SONOMA-Data/sonoma_raw.csv)
+data_path = ""
+
+# Put your path to the SONOMA csv here 
+#(e.g. //biostat-fs2-s/users/sherold/Documents/SONOMA-Data/sonoma_cleaned.csv)
+ouptut_path = ""
+
+
+
+
+#### LOAD ####
+df = read.csv(data_path) 
+library(dplyr)
+
+
+
+
+#### CLEAN (COMMENT ON EVERY LINE PLEASE) ####
+
+df = df[df$eligiblefull == 1,] # Select only fully eligible observations
+
+# etc., etc.
+
+
+
+
+#### SAVE ####
+
+write.csv(df, ouptut_path)
