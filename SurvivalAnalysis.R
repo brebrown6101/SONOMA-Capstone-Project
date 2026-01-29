ea1ee6bacf123162da57761a0a59604c2ecda656
brebrown6101 <bbrown34@uw.edu>
2026-01-28 20:55:28 -0800

    Creating Aalen-Johansen estimator

--- SurvivalAnalysis.R
+++ SurvivalAnalysis.R
@@ -5,6 +5,17 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 
+
+
+
+
+data_path = "C:/Bre/SONOMA/Sonoma_cleaned.csv"
+
+clean_s = read.csv(data_path, check.names = FALSE, row.names=1) 
+
+# colnames(clean_s)
+
+
 #subsetting data to just NOM patients
 NOM_patients <- clean_s[clean_s$initial_planoutcomes == 2,] 
 
@@ -22,47 +33,78 @@ NOM_patients <- clean_s[clean_s$initial_planoutcomes == 2,]
 
 #time until recurrence of appendicitis symptoms after NOM
 
-# Filter for 1s and remove NA values in nom_appyoutcomes. Further subsetting to just patients who received appy after NOM
-appy_after_nom_pts <- NOM_patients[NOM_patients$nom_appyoutcomes == 1 & !is.na(NOM_patients$nom_appyoutcomes), ]
 
-
-appy_after_nom_pts$time_of_dxoutcomes #date/time of  patients dx of appendicitis
-sum(is.na(appy_after_nom_pts$time_of_dxoutcomes)) #looking for NAs
-date_index <- gsub(" .*","", appy_after_nom_pts$time_of_dxoutcomes) #removing time part of date/time
-sum(is.na(appy_after_nom_pts$time_of_dxoutcomes)) #checking we have the same amount of NAs
+NOM_patients <- NOM_patients[!is.na(NOM_patients$time_of_dxoutcomes),]
+sum(is.na(NOM_patients$time_of_dxoutcomes)) #looking for NAs
+NOM_patients$time_of_dxoutcomes <- gsub(" .*","", NOM_patients$time_of_dxoutcomes) #removing time part of date/time
+sum(is.na(NOM_patients$time_of_dxoutcomes)) #checking we have the same amount of NAs
 #converting date_index to date object
-date_index <- as.Date(appy_after_nom_pts$time_of_dxoutcomes, format = "%m/%d/%Y")
-date_index
 
-#converting date of appendectomy after initial primary treatment with antibiotics to date object
-appy_time <- as.Date(appy_after_nom_pts$appy_int_dateoutcomes, format = "%m/%d/%Y") 
-appy_time <- appy_time[!is.na(appy_time)]
-appy_time #LET GRIFFEN KNOW -> 2015-05-01, also time where appendectomy was recorded of happening before NOM appointment
-#505 (1951-103) and 1655 (1960-5)
+NOM_patients$time_of_dxoutcomes <- as.Date(
+  NOM_patients$time_of_dxoutcomes,
+  format = "%m/%d/%Y"
+)
+
+class(NOM_patients$time_of_dxoutcomes)
+
+
+#converting date of appendectomy after NOM to date object, converting blanks as NAs
+NOM_patients$appy_int_dateoutcomes[
+  NOM_patients$appy_int_dateoutcomes == ""
+] <- NA
+
+NOM_patients$appy_int_dateoutcomes <- as.Date(
+  NOM_patients$appy_int_dateoutcomes,
+  format = "%m/%d/%Y"
+)
+
+class(NOM_patients$appy_int_dateoutcomes)
+
 
 #creating variable that is time in days from NOM decision to appy
-appy_after_nom_pts$time_to_appy <- appy_time - date_index
-appy_after_nom_pts$time_to_appy
+NOM_patients$time_to_appy <- NOM_patients$appy_int_dateoutcomes - NOM_patients$time_of_dxoutcomes
+#I thought they were only followed for a year after, but many of these times look way past 365 days so what time for censored individuals?
+
+#DONT NEED THIS IF WE GET NEW TIMES FOR TWO ERRORS
+NOM_patients <- NOM_patients[NOM_patients$time_to_appy > 0 | is.na(NOM_patients$time_to_appy), ] #DONT NEED THIS IF WE GET NEW TIMES
+
+#Creating censoring date as 01/01/2026
+censor_date <- as.Date("2026-01-01")
+
+#putting in time to censoring, creating time_to_event variable
+NOM_patients$time_to_event <- ifelse(
+  is.na(NOM_patients$time_to_appy),
+  as.numeric(censor_date - NOM_patients$time_of_dxoutcomes),
+  NOM_patients$time_to_appy
+)
+
+
+#setting NA values to 0 for censored
+NOM_patients$status <- ifelse(
+  is.na(NOM_patients$interval_typeoutcomes),
+  0,   # censored
+  NOM_patients$interval_typeoutcomes
+)
+
+NOM_patients$status <- as.numeric(NOM_patients$status)
+
+# #aalen-johansen estimator
+
+library(mstate)
+NOM_patients$event <- factor(NOM_patients$status, 0:2, c("censored", "Elective", "Urgent/Emergent"))
+fit <- survfit(Surv(time_to_event, event) ~1, data = NOM_patients)
+fit$transitions
+plot(fit, xmax = 900, col = 1:2, lwd = 2,
+     xlab = "Days from NOM", ylab = "Probability")
+legend(8, .2, c("Elective", "Urgent/Emergent"), lty = 1, lwd = 2, col = 1:2, bty = 'n')
 
 
 
 
-
-
-
-
-# dat_surv <- Surv(time = data$time, event = data$event)
 # km_fit <- survfit(dat_surv ~ 1, data = data, conf.type = "log-log")
 # 
 # ggsurvplot(km_fit, data = dat_surv, conf.int = T, ggtheme = theme_survminer(),
 #            title = "Kaplan-Meier Survival Curve", xlab = "Time", ylab = "Survival Probability", palette = "lightblue")
 # 
 # na_fit <- survfit(dat_surv ~ 1, data = data, type = "aalen")
-# 
-# 
-# #aalen-johansen estimator
-# data$event <- factor(data$event, levels = c(0:2), 
-#                      labels = c("Elective Appendectomy", "Non-elective appendectomy"))
-# 
-# km_fit <- survfit(dat_surv ~ 1, data = data, conf.type = "log-log")
-# 
+
